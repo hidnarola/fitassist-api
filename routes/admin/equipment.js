dd8a95fd2ede63943559a3c301fc18573e9d776e
ar-narola <ar@narola.email>
2018-03-27 12:48:28 +0530

    equipment changes

--- routes/admin/equipment.js
+++ routes/admin/equipment.js
@@ -20,9 +20,29 @@ var equipment_helper = require("../../helpers/equipment_helper");
  * @apiSuccess (Success 200) {Array} equipments Array of equipments document
  * @apiError (Error 4xx) {String} message Validation or error message.
  */
-router.get -
-  ("/",
-  async (req, res) => {
+router.get("/", async (req, res) => {
+    logger.trace("Get all equipment API called");
+    var resp_data = await equipment_helper.get_all_equipment();
+    if (resp_data.status == 0) {
+      logger.error("Error occured while fetching equipment = ", resp_data);
+      res.status(config.INTERNAL_SERVER_ERROR).json(resp_data);
+    } else {
+      logger.trace("Equipments got successfully = ", resp_data);
+      res.status(config.OK_STATUS).json(resp_data);
+    }
+  });
+
+  /**
+ * @api {get} /admin/equipment/equipment_id Equipment - Get equipment by ID
+ * @apiName Equipment - Get all
+ * @apiGroup Admin
+ *
+ * @apiHeader {String}  x-access-token Admin's unique access-key
+ *
+ * @apiSuccess (Success 200) {Array} equipments Array of equipments document
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.get("/", async (req, res) => {
     logger.trace("Get all equipment API called");
     var resp_data = await equipment_helper.get_all_equipment();
     if (resp_data.status == 0) {
@@ -152,6 +172,9 @@ router.put("/:equipment_id", async (req, res) => {
             errorMessage: "Category is required"
         }
     };
+
+    // Coming in few minutes
+    // ok
 
     req.checkBody(schema);
     var errors = req.validationErrors();
