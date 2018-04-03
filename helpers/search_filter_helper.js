801e8b597e3d501b3f69a3496c8ead7da619945b
ar-narola <ar@narola.email>
2018-04-03 14:07:59 +0530

    search filter update

--- helpers/search_filter_helper.js
+++ helpers/search_filter_helper.js
@@ -10,28 +10,44 @@ var filter_helper = {};
  */
 filter_helper.get_filtered_records = async (filter_obj) => {
 
+    console.log(filter_obj);
   queryObj={};
   if(filter_obj.columnFilter && filter_obj.columnFilter.length>0)
   {
-      queryObj['$and']=filter_obj.columnFilter;
+      queryObj=filter_obj.columnFilter;
   }
-  if(filter_obj.columnFilter && filter_obj.columnFilter.length>0)
+
+  equalTo={};
+  if(filter_obj.columnFilterEqual && filter_obj.columnFilterEqual.length>0)
   {
-      queryObj['$and']=filter_obj.columnFilter;
+    equalTo=filter_obj.columnFilterEqual;
   }
-  console.log(filter_obj.columnSort);
   skip=(filter_obj.pageSize*filter_obj.page);
     try {
         total_count= await  Exercise.count({},function(err,cnt){
             return cnt;
           });
-        var filtered_data = await Exercise.find(queryObj).or(filter_obj.columnFilterEqual).sort(filter_obj.columnSort).limit(filter_obj.pageSize).skip(skip).exec();
+        // var filtered_data = await Exercise.find(queryObj).sort(filter_obj.columnSort).limit(filter_obj.pageSize).skip(skip).exec();
+        
+        var searched_record_count = await Exercise.find({
+            $and : [
+                { $and : queryObj },
+                { $and : equalTo }
+            ]
+        } ).count();
+        
+        var filtered_data = await Exercise.find({
+            $and : [
+                { $and : queryObj },
+                { $and : equalTo }
+            ]
+        } ).sort(filter_obj.columnSort).limit(filter_obj.pageSize).skip(skip).exec();
 
         if (filtered_data) {
             filtered_data.forEach(element => {
             });
             //console.log(filtered_data.length);
-            return { "status": 1, "message": "filtered data is found","count":filtered_data.length,"filtered_total_pages":Math.ceil(total_count/filter_obj.pageSize), "filtered_exercises": filtered_data };
+            return { "status": 1, "message": "filtered data is found","count":searched_record_count,"filtered_total_pages":Math.ceil(total_count/filter_obj.pageSize), "filtered_exercises": filtered_data };
         } else {
             return { "status": 2, "message": "No filtered data available" };
         }
