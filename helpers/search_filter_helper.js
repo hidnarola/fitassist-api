1ee9d4a4195ba86f6e471b6f8c6e360978e18d08
ar-narola <ar@narola.email>
2018-04-03 14:19:09 +0530

    search filter helper

--- helpers/search_filter_helper.js
+++ helpers/search_filter_helper.js
@@ -8,53 +8,64 @@ var filter_helper = {};
  *          status 1 - If filtered data found, with filtered object
  *          status 2 - If filtered not found, with appropriate message
  */
-filter_helper.get_filtered_records = async (filter_obj) => {
-
-    console.log(filter_obj);
-  queryObj={};
-  if(filter_obj.columnFilter && filter_obj.columnFilter.length>0)
-  {
-      queryObj=filter_obj.columnFilter;
+filter_helper.get_filtered_records = async filter_obj => {
+  console.log(filter_obj);
+  queryObj = {};
+  if (filter_obj.columnFilter && filter_obj.columnFilter.length > 0) {
+    queryObj = filter_obj.columnFilter;
   }
 
-  equalTo={};
-  if(filter_obj.columnFilterEqual && filter_obj.columnFilterEqual.length>0)
-  {
-    equalTo=filter_obj.columnFilterEqual;
+  equalTo = {};
+  if (filter_obj.columnFilterEqual && filter_obj.columnFilterEqual.length > 0) {
+    equalTo = filter_obj.columnFilterEqual;
   }
-  skip=(filter_obj.pageSize*filter_obj.page);
-    try {
-        total_count= await  Exercise.count({},function(err,cnt){
-            return cnt;
-          });
-        // var filtered_data = await Exercise.find(queryObj).sort(filter_obj.columnSort).limit(filter_obj.pageSize).skip(skip).exec();
-        
-        var searched_record_count = await Exercise.find({
-            $and : [
-                { $and : queryObj },
-                { $and : equalTo }
-            ]
-        } ).count();
-        
-        var filtered_data = await Exercise.find({
-            $and : [
-                { $and : queryObj },
-                { $and : equalTo }
-            ]
-        } ).sort(filter_obj.columnSort).limit(filter_obj.pageSize).skip(skip).exec();
+  skip = filter_obj.pageSize * filter_obj.page;
+  try {
+    total_count = await Exercise.count({}, function(err, cnt) {
+      return cnt;
+    });
+    // var filtered_data = await Exercise.find(queryObj).sort(filter_obj.columnSort).limit(filter_obj.pageSize).skip(skip).exec();
 
-        if (filtered_data) {
-            filtered_data.forEach(element => {
-            });
-            //console.log(filtered_data.length);
-            return { "status": 1, "message": "filtered data is found","count":searched_record_count,"filtered_total_pages":Math.ceil(total_count/filter_obj.pageSize), "filtered_exercises": filtered_data };
-        } else {
-            return { "status": 2, "message": "No filtered data available" };
-        }
-    } catch (err) {
-        return { "status": 0, "message": "Error occured while filtering data", "error": err }
+    var andFilterArr = [];
+    if (queryObj && queryObj.length > 0) {
+      andFilterArr.push({ $and: queryObj });
     }
-}
 
+    if (equalTo && equalTo.length > 0) {
+      andFilterArr.push({ $and: equalTo });
+    }
+    var andFilterObj = {};
+    if (andFilterArr && andFilterArr.length > 0) {
+      andFilterObj = { $and: andFilterArr };
+    }
+    var searched_record_count = await Exercise.find(andFilterObj).count();
+
+    var filtered_data = await Exercise.find(andFilterObj)
+      .sort(filter_obj.columnSort)
+      .limit(filter_obj.pageSize)
+      .skip(skip)
+      .exec();
+
+    if (filtered_data) {
+      filtered_data.forEach(element => {});
+      //console.log(filtered_data.length);
+      return {
+        status: 1,
+        message: "filtered data is found",
+        count: searched_record_count,
+        filtered_total_pages: Math.ceil(total_count / filter_obj.pageSize),
+        filtered_exercises: filtered_data
+      };
+    } else {
+      return { status: 2, message: "No filtered data available" };
+    }
+  } catch (err) {
+    return {
+      status: 0,
+      message: "Error occured while filtering data",
+      error: err
+    };
+  }
+};
 
 module.exports = filter_helper;
