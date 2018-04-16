c9a391f260a4cd77d7a951a72807b51ca729bddd
ar-narola <ar@narola.email>
2018-04-16 10:28:45 +0530

    change in equipment_helper.js

--- helpers/equipment_helper.js
+++ helpers/equipment_helper.js
@@ -111,4 +111,53 @@ equipment_helper.delete_equipment_by_id = async (equipment_id) => {
     }
 }
 
+
+
+/*
+ * get_filtered_records is used to fetch all filtered data
+ * 
+ * @return  status 0 - If any internal error occured while fetching filtered data, with error
+ *          status 1 - If filtered data found, with filtered object
+ *          status 2 - If filtered not found, with appropriate message
+ */
+equipment_helper.get_filtered_records = async filter_obj => {
+  
+    skip = filter_obj.pageSize * filter_obj.page;
+    try {
+      var searched_record_count = await Equipment.aggregate([
+        {
+          $match: filter_object.columnFilter
+        }
+      ]);
+  
+      var filtered_data = await Equipment.aggregate([
+        {
+          $match: filter_object.columnFilter
+        },
+        { $skip: skip },
+        { $limit: filter_object.pageSize },
+        { $sort: filter_obj.columnSort }
+      ]);
+  
+      if (filtered_data) {
+        return {
+          status: 1,
+          message: "filtered data is found",
+          count: searched_record_count.length,
+          filtered_total_pages: Math.ceil(
+            searched_record_count.length / filter_obj.pageSize
+          ),
+          filtered_equipments: filtered_data
+        };
+      } else {
+        return { status: 2, message: "No filtered data available" };
+      }
+    } catch (err) {
+      return {
+        status: 0,
+        message: "Error occured while filtering data",
+        error: err
+      };
+    }
+  };
 module.exports = equipment_helper;
--- routes/admin/equipment.js
+++ routes/admin/equipment.js
@@ -9,6 +9,39 @@ var config = require("../../config");
 var logger = config.logger;
 
 var equipment_helper = require("../../helpers/equipment_helper");
+var common_helper = require("../../helpers/common_helper");
+
+
+
+/**
+ * @api {post} /admin/equipment/filter Filter
+ * @apiName Filter
+ * @apiGroup Equipment
+ * 
+ * @apiHeader {String}  Content-Type application/json
+ * @apiHeader {String}  x-access-token Admin's unique access-key
+ * 
+ * @apiParam {Object} columnFilter columnFilter Object for filter data
+ * @apiParam {Object} columnSort columnSort Object for Sorting Data
+ * @apiParam {Object} columnFilterEqual columnFilterEqual Object for select box
+ * @apiParam {Number} pageSize pageSize
+ * @apiParam {Number} page page number
+ * @apiSuccess (Success 200) {JSON} filtered_equipments filtered details
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+
+router.post("/filter", async (req, res) => {
+  
+    filter_object = common_helper.changeObject(req.body);
+    let filtered_data = await equipment_helper.get_filtered_records(filter_object);
+    if (filtered_data.status === 0) {
+      logger.error("Error while fetching searched data = ", filtered_data);
+      return res.status(config.BAD_REQUEST).json({ filtered_data });
+    } else {
+      return res.status(config.OK_STATUS).json(filtered_data);
+    }
+  });
+  
 
 /**
  * @api {get} /admin/equipment Get all
@@ -291,5 +324,7 @@ router.delete("/:equipment_id", async (req, res) => {
     res.status(config.OK_STATUS).json(equipment_data);
   }
 });
+
+
 
 module.exports = router;
