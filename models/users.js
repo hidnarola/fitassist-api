f95379a5c84197208251b5a930910a068e022a07
ar-narola <ar@narola.email>
2018-04-17 18:38:44 +0530

    change in users.js

--- models/users.js
+++ models/users.js
@@ -49,22 +49,6 @@ var UserSchema = new Schema(
   { versionKey: false }
 );
 
-UserSchema.pre("save", function(next) {
-  var user = this;
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified("password")) return next();
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
 
 // Compile model from schema
 var User = mongoose.model("users", UserSchema, "users");
