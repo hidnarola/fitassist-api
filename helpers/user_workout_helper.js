1fb3f18c76acf08150ac2c1c03cdf333cf283165
ar-narola <ar@narola.email>
2018-05-22 17:25:47 +0530

    user workout data with using populate

--- helpers/user_workout_helper.js
+++ helpers/user_workout_helper.js
@@ -10,18 +10,28 @@ var user_workout_helper = {};
  *          status 1 - If user workouts data found, with user workouts object
  *          status 2 - If user workouts not found, with appropriate message
  */
-user_workout_helper.get_user_workout = async (userId) => {
-    try {
-        var user_workout = await UserWorkout.find(userId);
-        if (user_workout) {
-            return { "status": 1, "message": "user workouts details found", "user_workouts": user_workout };
-        } else {
-            return { "status": 2, "message": "user workouts not found" };
-        }
-    } catch (err) {
-        return { "status": 0, "message": "Error occured while finding user workouts", "error": err }
+user_workout_helper.get_user_workout = async userId => {
+  try {
+    var user_workout = await UserWorkout.find().populate({
+      path: "schedule.exerciseId",
+      populate: { path: 'exercise' }
+    });
+    if (user_workout) {
+      return {
+        status: 1,
+        message: "user workouts details found",
+        user_workouts: user_workout
+      };
+    } else {
+      return { status: 2, message: "user workouts not found" };
     }
+  } catch (err) {
+    return {
+      status: 0,
+      message: "Error occured while finding user workouts",
+      error: err
+    };
+  }
 };
-
 
 module.exports = user_workout_helper;
