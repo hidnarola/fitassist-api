8a0e7c0798648d4d1e476bf87e87e10934a69fc7
ar-narola <ar@narola.email>
2018-07-12 11:27:35 +0530

    changes in doc

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-07-11T06:32:38.967Z",
+    "time": "2018-07-12T05:57:16.732Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
