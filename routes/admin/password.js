02d2a159393f9dba43e04a6e50bba7bb457144aa
ar-narola <ar@narola.email>
2018-09-01 12:46:09 +0530

    changes in password

--- routes/admin/password.js
+++ routes/admin/password.js
@@ -25,7 +25,6 @@ router.put("/", async (req, res) => {
 
   logger.trace("API - Admin change password  called");
   logger.debug("req.body = ", req.body);
-  var token = req.headers['x-access-token'];
   var resp = null;
   var schema = {
     password: {
@@ -46,116 +45,90 @@ router.put("/", async (req, res) => {
 
   var errors = req.validationErrors();
   if (!errors) {
-    if (token) {
-      resp = jwt.verify(token, config.ACCESS_TOKEN_SECRET_KEY, function (err, decoded) {
-        if (err) {
-          return {
-            status: 0,
-            err: err,
-          };
-        } else {
-          return {
-            status: 1,
-            data: decoded.email,
-          };
-        }
+    logger.trace("Valid request of admin change password");
+    // Checking for user availability
+    logger.trace("Checking for user availability");
+    let user_resp = await admin_helper.get_admin_by_id(req.userInfo.id);
+    logger.trace("User checked resp = ", user_resp);
+
+    if (user_resp.status === 0) {
+      logger.error(
+        "Error in finding user by email in admin change password API. Err = ",
+        user_resp.err
+      );
+      res.status(config.INTERNAL_SERVER_ERROR).json({
+        status: 0,
+        message: "Something went wrong while finding admin",
+        error: user_resp.error
       });
-    } else {
-      return res.status(config.UNAUTHORIZED).json({
-        message: 'Unauthorized access'
-      });
-    }
+    } else if (user_resp.status === 1) {
+      logger.trace("admin data found. Executing next instruction");
+      // Checking password
 
-    if (resp && resp.status && resp.status === 1) {
-      logger.trace("Valid request of admin change password");
-      // Checking for user availability
-      logger.trace("Checking for user availability");
-      let user_resp = await admin_helper.get_admin_by_email(resp.data);
-      logger.trace("User checked resp = ", user_resp);
+      common_helper.hashPassword.call({
+          password: req.body.password,
+          hash: user_resp.admin.password
+        },
+        async password_resp => {
 
-      if (user_resp.status === 0) {
-        logger.error(
-          "Error in finding user by email in admin change password API. Err = ",
-          user_resp.err
-        );
+          logger.trace("password resp = ", password_resp);
+          if (password_resp.status === 0 || password_resp.res === false) {
+            res.status(config.BAD_REQUEST).json({
+              status: 0,
+              message: "Old password does not match"
+            });
+          } else {
+            if (user_resp.admin.status) {
+              // Generate token
+              bcrypt.genSalt(SALT_WORK_FACTOR, async function (err, salt) {
+                if (err) {
+                  console.log("Error", err)
+                };
+                bcrypt.hash(req.body.newPassword, salt, async function (err, hash) {
+                  if (err) {
+                    console.log("Error", err);
+                  }
+                  let update_resp = await admin_helper.update_admin_by_id(
+                    user_resp.admin._id, {
+                      password: hash,
+                    }
+                  );
+                  if (update_resp.status === 1) {
+                    res.status(config.OK_STATUS).json({
+                      status: 1,
+                      message: "Password Changed successful",
+                    });
 
-        res.status(config.INTERNAL_SERVER_ERROR).json({
-          status: 0,
-          message: "Something went wrong while finding admin",
-          error: user_resp.error
-        });
-      } else if (user_resp.status === 1) {
-        logger.trace("admin data found. Executing next instruction");
-
-        // Checking password
-        common_helper.hashPassword.call({
-            password: req.body.password,
-            hash: user_resp.admin.password
-          },
-          async password_resp => {
-
-            logger.trace("password resp = ", password_resp);
-            if (password_resp.status === 0 || password_resp.res === false) {
-              res.status(config.BAD_REQUEST).json({
-                status: 0,
-                message: "Old password does not match"
+                  } else {
+                    res.status(config.INTERNAL_SERVER_ERROR).json({
+                      status: 2,
+                      message: "Password does not changed",
+                    });
+                  }
+                });
               });
             } else {
-              if (user_resp.admin.status) {
-                // Generate token
-                bcrypt.genSalt(SALT_WORK_FACTOR, async function (err, salt) {
-                  if (err) {
-                    console.log("Error", err)
-                  };
-                  bcrypt.hash(req.body.newPassword, salt, async function (err, hash) {
-                    if (err) {
-                      console.log("Error", err);
-                    }
-                    let update_resp = await admin_helper.update_admin_by_id(
-                      user_resp.admin._id, {
-                        password: hash,
-                      }
-                    );
-                    if (update_resp.status === 1) {
-                      res.status(config.OK_STATUS).json({
-                        status: 1,
-                        message: "Password Changed successful",
-                      });
-
-                    } else {
-                      res.status(config.INTERNAL_SERVER_ERROR).json({
-                        status: 2,
-                        message: "Password does not changed",
-                      });
-                    }
-                  });
+              logger.trace("Admin account is not active");
+              res
+                .status(config.BAD_REQUEST)
+                .json({
+                  status: 0,
+                  message: "Account is not active"
                 });
-              } else {
-                logger.trace("Admin account is not active");
-                res
-                  .status(config.BAD_REQUEST)
-                  .json({
-                    status: 0,
-                    message: "Account is not active"
-                  });
-              }
             }
           }
-        );
-      } else {
-        logger.info("Account doesn't exist.");
-        res
-          .status(config.BAD_REQUEST)
-          .json({
-            status: 0,
-            message: "Invalid email address"
-          });
-      }
+        }
+      );
     } else {
-      res.status(config.BAD_REQUEST).json({
-        message: "Invalid token"
-      });
+      logger.info("Account doesn't exist.");
+      res
+        .status(config.BAD_REQUEST)
+        .json({
+          status: 0,
+          message: "Invalid email address"
+        });
     }
+
   } else {
     logger.error("Validation Error = ", errors);
     res.status(config.VALIDATION_FAILURE_STATUS).json({
