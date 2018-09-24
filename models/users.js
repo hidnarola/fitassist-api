82e55be27c0555a79a8e139ad955f1feef2b9b2b
ar-narola <ar@narola.email>
2018-09-24 17:26:02 +0530

    dashboad api added in admin route

--- models/users.js
+++ models/users.js
@@ -6,37 +6,87 @@ var Schema = mongoose.Schema;
 var bcrypt = require("bcrypt");
 var SALT_WORK_FACTOR = 10;
 
-var UserSchema = new Schema(
-  {
-    authUserId: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String },
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    mobileNumber: { type: String },
-    gender: {
-      type: String,
-      enum: ["male", "female", "transgender"],
-      default: "male"
-    },
-    dateOfBirth: { type: Date, default: null },
-    height: { type: Number, default: 0 },
-    weight: { type: Number, default: 0 },
-    goal: { type: Object, default: null },
-    workoutLocation: {
-      type: String,
-      enum: ["gym", "home"],
-      default: "gym"
-    },
-    avatar: { type: String },
-    aboutMe: { type: String },
-    status: { type: Number, default: 1 },
-    isDeleted: { type: Number, default: 0 },
-    createdAt: { type: Date, default: Date.now },
-    modifiedAt: { type: Date, default: Date.now }
+var UserSchema = new Schema({
+  authUserId: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { versionKey: false }
-);
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    default: null
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  mobileNumber: {
+    type: String,
+    default: null
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "transgender"],
+    default: "male"
+  },
+  dateOfBirth: {
+    type: Date,
+    default: null
+  },
+  height: {
+    type: Number,
+    default: 0
+  },
+  weight: {
+    type: Number,
+    default: 0
+  },
+  goal: {
+    type: Object,
+    default: null
+  },
+  workoutLocation: {
+    type: String,
+    enum: ["gym", "home"],
+    default: "gym"
+  },
+  avatar: {
+    type: String,
+    default: null
+  },
+  aboutMe: {
+    type: String,
+    default: null
+  },
+  status: {
+    type: Number,
+    default: 1
+  },
+  isDeleted: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  versionKey: false
+});
 
 // Compile model from schema
 var User = mongoose.model("users", UserSchema, "users");
--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var config = require("../../config");
 var logger = config.logger;
+var moment = require("moment");
 var user_helper = require("../../helpers/user_helper");
 var exercise_helper = require("../../helpers/exercise_helper");
 var user_workouts_helper = require("../../helpers/user_workouts_helper");
@@ -30,40 +31,140 @@ router.post("/", async (req, res) => {
     totalUsers: null,
     topUsers: null
   };
+  var last2StepData;
+  var tmp;
+  var perChange;
+  var current = await moment(req.body.end).endOf('day').utc(0);
+  var start = await moment(req.body.start).startOf('day').utc(0);
+  var days = current.diff(start, 'days');
+  var previousStart = await moment(start).subtract(days, "day");
+  var totalUsers = await user_helper.count_users({
+    createdAt: {
+      $gte: start,
+      $lte: current
+    }
+  });
 
-  var totalUsers = await user_helper.count_users();
+  last2StepData = await user_helper.count_users({
+    createdAt: {
+      $gte: previousStart,
+      $lte: start
+    }
+  });
+
+  tmp = (totalUsers.count - last2StepData.count);
+  perChange = parseFloat(((tmp / totalUsers.count) * 100).toFixed(2));
+
   if (totalUsers.status === 1) {
-    returnObject.totalUsers = totalUsers.count;
+    returnObject.totalUsers = {
+      total: totalUsers.count,
+      perChange,
+      days
+    };
   }
 
-  var totalExercises = await exercise_helper.count_exercises();
+  var totalExercises = await exercise_helper.count_exercises({
+    createdAt: {
+      $gte: start,
+      $lte: current
+    }
+  });
+  last2StepData = await exercise_helper.count_exercises({
+    createdAt: {
+      $gte: previousStart,
+      $lte: start
+    }
+  });
+
+  tmp = (totalExercises.count - last2StepData.count);
+  perChange = parseFloat(((tmp / totalExercises.count) * 100).toFixed(2));
   if (totalExercises.status === 1) {
-    returnObject.totalExercises = totalExercises.count;
+    returnObject.totalExercises = {
+      total: totalExercises.count,
+      perChange,
+      days
+    };
   }
 
-  var totalFitnessTest = await test_exercise_helper.count_test_exercises();
+  var totalFitnessTest = await test_exercise_helper.count_test_exercises({
+    createdAt: {
+      $gte: start,
+      $lte: current
+    }
+  });
+
+  last2StepData = await test_exercise_helper.count_test_exercises({
+    createdAt: {
+      $gte: previousStart,
+      $lte: start
+    }
+  });
+
+  tmp = (totalFitnessTest.count - last2StepData.count);
+  perChange = parseFloat(((tmp / totalFitnessTest.count) * 100).toFixed(2));
   if (totalFitnessTest.status === 1) {
-    returnObject.totalFitnessTest = totalFitnessTest.count;
+    returnObject.totalFitnessTest = {
+      total: totalFitnessTest.count,
+      perChange,
+      days
+    };
   }
 
-  var totalProgram = await user_program_helper.count_total_programs();
+  var totalProgram = await user_program_helper.count_total_programs({
+    createdAt: {
+      $gte: start,
+      $lte: current
+    }
+  });
+  last2StepData = await user_program_helper.count_total_programs({
+    createdAt: {
+      $gte: previousStart,
+      $lte: start
+    }
+  });
+
+  tmp = (totalProgram.count - last2StepData.count);
+  perChange = parseFloat(((tmp / totalProgram.count) * 100).toFixed(2));
   if (totalProgram.status === 1) {
-    returnObject.totalProgram = totalProgram.count;
+    returnObject.totalProgram = {
+      total: totalProgram.count,
+      perChange,
+      days
+    };
   }
 
   var completedExercises = await user_workouts_helper.count_all_workouts({
+    createdAt: {
+      $gte: start,
+      $lte: current
+    },
     isCompleted: 1
   });
 
-  if (completedExercises.status === 1) {
-    returnObject.completedExercises = completedExercises.count;
-  }
+  last2StepData = await user_workouts_helper.count_all_workouts({
+    createdAt: {
+      $gte: previousStart,
+      $lte: start
+    },
+    isCompleted: 1
+  });
 
+  tmp = (completedExercises.count - last2StepData.count);
+  perChange = parseFloat(((tmp / completedExercises.count) * 100).toFixed(2));
+
+  if (completedExercises.status === 1) {
+    returnObject.completedExercises = {
+      total: completedExercises.count,
+      perChange,
+      days
+    };
+  }
   return res.status(config.OK_STATUS).send({
     status: 1,
     message: "Dashboard record found",
     data: returnObject
-  })
+  });
+
 });
 
 
