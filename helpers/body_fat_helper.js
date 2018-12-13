5146df89a33b767709499e7eb7dbe377e01368bb
sbnarola <sb@narola.email>
2018-12-13 14:34:13 +0530

    Changes in body fat log api

--- helpers/body_fat_helper.js
+++ helpers/body_fat_helper.js
@@ -126,5 +126,36 @@ body_fat_helper.save_body_fat_log = async (bodyFatObject, id = false) => {
   }
 };
 
+/*
+ * get_body_fat_log_by_date is used to fetch bodymeasurement by date and user id
+ * @return  status 0 - If any internal error occured while fetching bodymeasurement data, with error
+ *          status 1 - If bodymeasurement data found, with bodymeasurement object
+ *          status 2 - If bodymeasurement not found, with appropriate message
+ */
+body_fat_helper.get_body_fat_log_by_date = async (cond) => {
+  try {
+    var body_fat_log = await BodyFatLogs.findOne(cond);
+    if (body_fat_log) {
+      return {
+        status: 1,
+        message: "body fat log found",
+        body_fat_log: body_fat_log
+      };
+    } else {
+      return {
+        status: 2,
+        message: "No body_fat_log available",
+        body_fat_log: null
+      };
+    }
+  } catch (err) {
+    return {
+      status: 0,
+      message: "Error occured while finding body_fat_log",
+      error: err
+    };
+  }
+};
+
 
 module.exports = body_fat_helper;
