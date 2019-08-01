245d1905cedee38b25136bc51a3bf180de487b78
Kishan Patel <ksp@narola.email>
2019-08-01 17:56:24 +0530

    changes in router file

--- routes/user/meals.js
+++ routes/user/meals.js
@@ -9,42 +9,113 @@ var common_helper = require("../../helpers/common_helper");
 var jwtDecode = require("jwt-decode");
 
 //  post meal
-router.post("/search", async (req, res) => {
-  var re = new RegExp(req.body.name, "i");
-  value = {
-    $regex: re
-  };
-  var projectObj = {
-    $project: {
-      title: 1,
-      meals_type: 1,
-      meals_visibility: 1,
-      ingredientsIncluded: 1,
-      userId: 1,
-      image: 1
+router.post("/", async (req, res) => {
+  var decoded = jwtDecode(req.headers["authorization"]);
+  var authUserId = decoded.sub;
+  var schema = {
+    title: {
+      notEmpty: true,
+      isLength: {
+        errorMessage: "title should be between 2 to 50 characters",
+        options: {
+          min: 2,
+          max: 50
+        }
+      },
+      errorMessage: "title of meal is required"
+    },
+    meals_type: {
+      notEmpty: true,
+      errorMessage: "meals type is required"
+    },
+    meals_visibility: {
+      notEmpty: true,
+      errorMessage: "meals_visibility is required"
+    },
+    ingredientsIncluded: {
+      notEmpty: true,
+      errorMessage: "ingredients is required"
     }
   };
-  var searchObj = {
-    $match: {
-      title: value
+
+  if (req.body.meals_visibility && req.body.meals_visibility === "public") {
+    schema.instructions = {
+      notEmpty: true,
+      errorMessage: "Instruction is required"
+    };
+  }
+  req.checkBody(schema);
+  var errors = req.validationErrors();
+
+  if (!errors) {
+    let meals_obj = {
+      title: req.body.title,
+      notes: req.body.notes,
+      meals_type: req.body.meals_type,
+      meals_visibility: req.body.meals_visibility,
+      instructions: req.body.instructions,
+      ingredientsIncluded:
+        typeof req.body.ingredientsIncluded === "string"
+          ? JSON.parse(req.body.ingredientsIncluded)
+          : req.body.ingredientsIncluded,
+      userId: authUserId
+    };
+
+    //image upload
+    let filename;
+    let file;
+    if (req.files && req.files["meal_img"]) {
+      file = req.files["meal_img"];
+      extention = path.extname(file.name);
+      filename = "meal_" + new Date().getTime() + extention;
+      meals_obj.image = "uploads/meal/" + filename;
     }
-  };
-  var start = {
-    $skip: parseInt(req.body.start ? req.body.start : 0)
-  };
-  var offset = {
-    $limit: parseInt(req.body.offset ? req.body.offset : 10)
-  };
-  let resp_data = await meals_helper.search_meal(
-    projectObj,
-    searchObj,
-    start,
-    offset
-  );
-  if (resp_data.status == 1) {
-    res.status(config.OK_STATUS).json(resp_data);
+    if (req.files && req.files["meal_img"]) {
+      var dir = "./uploads/meal";
+      var mimetype = ["image/png", "image/jpeg", "image/jpg"];
+
+      if (mimetype.indexOf(file.mimetype) != -1) {
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+        file.mv(dir + "/" + filename, function(err) {
+          if (err) {
+            logger.error("There was an issue in uploading image");
+            return res.send({
+              status: config.MEDIA_ERROR_STATUS,
+              err: "There was an issue in uploading image"
+            });
+          } else {
+            logger.trace("image has been uploaded. Image name = ", filename);
+          }
+        });
+      } else {
+        logger.error("Image format is invalid");
+        return res.send({
+          status: config.MEDIA_ERROR_STATUS,
+          err: "Image format is invalid",
+          message: "Image format is invalid"
+        });
+      }
+    } else {
+      logger.info("Image not available to upload. Executing next instruction");
+    }
+
+    // insert recent ingredient
+    let meal_data = await meals_helper.insert_meal(meals_obj);
+    if (meal_data.status === 0) {
+      logger.error("Error while inserting meal data = ", meal_data);
+      return res.status(config.BAD_REQUEST).json({
+        meal_data
+      });
+    } else {
+      return res.status(config.OK_STATUS).json(meal_data);
+    }
   } else {
-    res.status(config.BAD_REQUEST).json(resp_data);
+    logger.error("Validation Error = ", errors);
+    return res.status(config.VALIDATION_FAILURE_STATUS).json({
+      message: errors
+    });
   }
 });
 
