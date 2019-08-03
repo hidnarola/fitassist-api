f3a8333984ecf42ff3d26a2ed222601756004107
Kishan Patel <ksp@narola.email>
2019-08-03 12:31:06 +0530

    add user_meal_helper || edit user_meals

--- routes/user/user_meals.js
+++ routes/user/user_meals.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var config = require("../../config");
 var logger = config.logger;
 var jwtDecode = require("jwt-decode");
+var meal_helper = require("../../helpers/user_helper");
 
 router.post("/", async (req, res) => {
   var decoded = jwtDecode(req.headers["authorization"]);
@@ -12,15 +13,29 @@ router.post("/", async (req, res) => {
       notEmpty: true,
       errorMessage: "meals is required"
     },
-    meals_visibility: {
+    date: {
       notEmpty: true,
-      errorMessage: "meals_visibility is required"
-    },
-    ingredientsIncluded: {
-      notEmpty: true,
-      errorMessage: "ingredients is required"
+      errorMessage: "date is required"
     }
   };
+  var meals_obj = {
+    meals: req.body.meals,
+    date: req.body.date,
+    userId: authUserId
+  };
+  req.checkBody(schema);
+  var errors = req.validationErrors();
+  if (!errors) {
+    let meal_data = await meal_helper.insert_meal(meals_obj);
+    if (meal_data.status === 0) {
+      logger.error("Error while inserting meal data = ", meal_data);
+      return res.status(config.BAD_REQUEST).json({
+        meal_data
+      });
+    } else {
+      return res.status(config.OK_STATUS).json(meal_data);
+    }
+  }
 });
 
 module.exports = router;
