4d7e540b1da5b8cdaccf6a2c3a003dd478026147
Kishan Patel <ksp@narola.email>
2019-08-05 17:50:38 +0530

    edit user meals

--- helpers/user_meal_helper.js
+++ helpers/user_meal_helper.js
@@ -5,11 +5,68 @@ meals_helper.insert_meal = async meal_obj => {
   let meal = new UserMeals(meal_obj);
   try {
     let meal_data = await meal.save();
+    if (meal_data) {
+      return { status: 1, message: "meal inserted", meal: meal_data };
+    } else {
+      return {
+        status: 0,
+        message: "Error occured while inserting meal",
+        error: err
+      };
+    }
   } catch (err) {
-    return { status: 1, message: "meal inserted", meal: meal_data };
     return {
       status: 0,
-      message: "Error occured while inserting meal",
+      message: "Error occured while finding meal",
+      error: err
+    };
+  }
+};
+
+meals_helper.get_logdata_by_userid = async id => {
+  try {
+    var logdata = await UserMeals.find(id);
+    //        var logdata = await Measurement.aggregate(id);
+    if (logdata) {
+      return {
+        status: 1,
+        message: "logdata found",
+        logdates: logdata
+      };
+    } else {
+      return {
+        status: 2,
+        message: "No logdata available"
+      };
+    }
+  } catch (err) {
+    return {
+      status: 0,
+      message: "Error occured while finding logdata",
+      error: err
+    };
+  }
+};
+
+meals_helper.get_user_meal_by_id = async obj => {
+  try {
+    var user_meal = await UserMeals.find(obj);
+    if (user_meal) {
+      return {
+        status: 1,
+        message: "meal found",
+        userMeals: user_meal
+      };
+    } else {
+      return {
+        status: 2,
+        message: "No meal available"
+      };
+    }
+  } catch (error) {
+    return {
+      status: 0,
+      message: "Error occured while finding logdata",
       error: err
     };
   }
--- routes/user/user_meals.js
+++ routes/user/user_meals.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var config = require("../../config");
 var logger = config.logger;
 var jwtDecode = require("jwt-decode");
-var meal_helper = require("../../helpers/user_helper");
+var meals_helper = require("../../helpers/user_meal_helper");
+var moment = require("moment");
 
 router.post("/", async (req, res) => {
   var decoded = jwtDecode(req.headers["authorization"]);
@@ -26,7 +27,7 @@ router.post("/", async (req, res) => {
   req.checkBody(schema);
   var errors = req.validationErrors();
   if (!errors) {
-    let meal_data = await meal_helper.insert_meal(meals_obj);
+    let meal_data = await meals_helper.insert_meal(meals_obj);
     if (meal_data.status === 0) {
       logger.error("Error while inserting meal data = ", meal_data);
       return res.status(config.BAD_REQUEST).json({
@@ -35,6 +36,96 @@ router.post("/", async (req, res) => {
     } else {
       return res.status(config.OK_STATUS).json(meal_data);
     }
+  }
+});
+
+router.post("/get_log_dates_by_date", async (req, res) => {
+  var decoded = jwtDecode(req.headers["authorization"]);
+  var schema = {
+    logDate: {
+      notEmpty: true,
+      errorMessage: "log Date is required"
+    }
+  };
+  req.checkBody(schema);
+  var errors = req.validationErrors();
+
+  if (!errors) {
+    var authUserId = decoded.sub;
+    var check = await moment(req.body.logDate).utc(0);
+    var startCheck = await moment(check).subtract(2, "month");
+    var endCheck = await moment(check).add(2, "month");
+
+    var log_data = await meals_helper.get_logdata_by_userid({
+      userId: authUserId,
+      date: {
+        $gte: startCheck,
+        $lte: endCheck
+      }
+    });
+
+    if (log_data.status != 0) {
+      res.status(config.OK_STATUS).json(log_data);
+    } else {
+      return res.status(config.BAD_REQUEST).json({
+        log_data
+      });
+    }
+  } else {
+    logger.error("Validation Error = ", errors);
+    res.status(config.VALIDATION_FAILURE_STATUS).json({
+      message: errors
+    });
+  }
+});
+
+router.post("/get_by_id_user_meal", async (req, res) => {
+  logger.trace("Get measurement by authUserId and logDate API called");
+  var decoded = jwtDecode(req.headers["authorization"]);
+  var authUserId = decoded.sub;
+  var logDate = req.body.logDate;
+  var schema = {
+    logDate: {
+      notEmpty: true,
+      errorMessage: "Log Date is required"
+    }
+  };
+  req.checkBody(schema);
+  var errors = req.validationErrors();
+
+  if (!errors) {
+    var startdate = moment(logDate).utcOffset(0);
+    startdate.toISOString();
+    startdate.format();
+
+    var enddate = moment(logDate)
+      .utcOffset(0)
+      .add(23, "hours")
+      .add(59, "minutes");
+    enddate.toISOString();
+    enddate.format();
+
+    let cond = {
+      userId: authUserId,
+      logDate: {
+        $gte: startdate,
+        $lte: enddate
+      }
+    };
+
+    var resp_data = await meals_helper.get_user_meal_by_id(cond);
+    if (resp_data.status != 0) {
+      res.status(config.OK_STATUS).json(resp_data);
+    } else {
+      return res.status(config.BAD_REQUEST).json({
+        resp_data
+      });
+    }
+  } else {
+    logger.error("Validation Error = ", errors);
+    res.status(config.VALIDATION_FAILURE_STATUS).json({
+      message: errors
+    });
   }
 });
 
